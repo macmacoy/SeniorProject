4d20f7ba5dcaaa4dbf8228f6d12784eab000f4e4
Mac Macoy <mmacoy@ufl.edu>
2018-02-06 15:21:32 -0500

    CommandLineDemo update

--- CommandLineDemo.py
+++ CommandLineDemo.py
@@ -1,36 +1,58 @@
 from Song import Song
+from ChordRecognizer import madmomChord
 import time
 import os
 
 my_song = Song("tests/test_song.json")
 
+os.system('clear')
 print ("Song: " + my_song.name)
 print ("Artist: " + my_song.artist)
 print ("Tempo: " + str(my_song.tempo))
 print ("Capo: " + str(my_song.capo))
-
+print ("")
 print ("Starting in ...")
-for i in range(3,0):
-	print ("   " + i + "   ")
+for i in range(0,3):
+	print ("   " + str(3-i) + "   ")
 	time.sleep(1)
 
 chords = my_song.chords
 lyrics = my_song.lyrics
 
+tab = " " * 50
+def draw(hit):
+	os.system('clear')
+	print (lyrics[lyricIndex]["lyric"] + tab[len(lyrics[lyricIndex]["lyric"]):] + chords[chordIndex]["chord"] + " <----")
+	if hit:
+		print(tab[4:] + "-NICE!-")
+	else:
+		print(tab[1:] + "-X-")
+	print("")
+	if lyrics[lyricIndex]["end"] != my_song.duration and chords[chordIndex]["end"] != my_song.duration:
+		# print("   next:   " + tab[9:] + "next:")
+		print(lyrics[lyricIndex+1]["lyric"] + tab[len(lyrics[lyricIndex+1]["lyric"]):] + chords[chordIndex+1]["chord"])
+	elif lyrics[lyricIndex]["end"] != my_song.duration:
+		# print(tab[2:] + "next:")
+		print(tab + lyrics[lyricIndex+1]["lyric"])
+	elif chords[chordIndex]["end"] != my_song.duration:
+		# print("   next:   ")
+		print(tab + chords[chordIndex+1]["chord"])
+
 lyricIndex = 0
 chordIndex = 0
+hit = False
 start = time.time()
-print (lyrics[0]["lyric"])
-print (" ------ " + chords[0]["chord"] + " ------ ")
 while(time.time() < start + my_song.duration):
+	draw(hit)
 	now = time.time() - start
-	if(now > chords[chordIndex]["end"]):
-		chordIndex += 1
-		os.system('clear')
-		print (lyrics[lyricIndex]["lyric"])
-		print (" ------ " + chords[chordIndex]["chord"] + " ------ "  + " <------- now")
-	if(now > lyrics[lyricIndex]["end"]):
-		lyricIndex += 1
-		os.system('clear')
-		print (lyrics[lyricIndex]["lyric"])
-		print (" ------ " + chords[chordIndex]["chord"] + " ------ "  + " <------- now")
+	while(now < chords[chordIndex]["end"]):
+		if (now > lyrics[lyricIndex]["end"]):
+			lyricIndex += 1
+			draw(hit)
+		if hit == False:
+			if madmomChord() == chords[chordIndex]["chord"]:
+				hit = True
+				redraw(hit)
+		now = time.time() - start
+	chordIndex += 1
+	hit = False
--- Song.py
+++ Song.py
@@ -12,6 +12,7 @@ class Song(object):
 		    self.duration = data.get("song").get("duration")
 		    self.tempo = data.get("song").get("tempo")
 		    self.capo = data.get("song").get("capo")
+
 		    chord_data = data.get("song").get("chords")
 		    self.chords = []
 		    for i in range(0, len(chord_data)):
@@ -19,6 +20,7 @@ class Song(object):
 		    		self.chords.append({"start":chord_data[i]["timestamp"], "end":chord_data[i+1]["timestamp"], "chord":chord_data[i]["chord"]})
 		    	else:
 		    		self.chords.append({"start":chord_data[i]["timestamp"], "end":self.duration, "chord":chord_data[i]["chord"]})
+		    
 		    lyric_data = data.get("song").get("lyrics")
 		    self.lyrics = []
 		    for i in range(0, len(lyric_data)):
