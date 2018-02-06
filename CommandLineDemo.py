be79dbf38c088360ffb351377cc5b47b7c13a7ad
Mac Macoy <mmacoy@ufl.edu>
2018-02-05 20:23:29 -0500

    changes on demo

--- CommandLineDemo.py
+++ CommandLineDemo.py
@@ -1,5 +1,6 @@
 from Song import Song
 import time
+import os
 
 my_song = Song("tests/test_song.json")
 
@@ -8,17 +9,28 @@ print ("Artist: " + my_song.artist)
 print ("Tempo: " + str(my_song.tempo))
 print ("Capo: " + str(my_song.capo))
 
+print ("Starting in ...")
+for i in range(3,0):
+	print ("   " + i + "   ")
+	time.sleep(1)
+
 chords = my_song.chords
 lyrics = my_song.lyrics
 
 lyricIndex = 0
 chordIndex = 0
 start = time.time()
+print (lyrics[0]["lyric"])
+print (" ------ " + chords[0]["chord"] + " ------ ")
 while(time.time() < start + my_song.duration):
 	now = time.time() - start
-	if(now >= chords[chordIndex+1]["timestamp"]):
-		print ("Chord : " + chords[chordIndex]["chord"])
+	if(now > chords[chordIndex]["end"]):
 		chordIndex += 1
-	if(now >= lyrics[lyricIndex+1]["timestamp"]):
-		print ("Lyric : " + lyrics[lyricIndex]["lyric"])
+		os.system('clear')
+		print (lyrics[lyricIndex]["lyric"])
+		print (" ------ " + chords[chordIndex]["chord"] + " ------ "  + " <------- now")
+	if(now > lyrics[lyricIndex]["end"]):
 		lyricIndex += 1
+		os.system('clear')
+		print (lyrics[lyricIndex]["lyric"])
+		print (" ------ " + chords[chordIndex]["chord"] + " ------ "  + " <------- now")
--- Song.py
+++ Song.py
@@ -12,8 +12,20 @@ class Song(object):
 		    self.duration = data.get("song").get("duration")
 		    self.tempo = data.get("song").get("tempo")
 		    self.capo = data.get("song").get("capo")
-		    self.chords = data.get("song").get("chords")
-		    self.lyrics = data.get("song").get("lyrics")
+		    chord_data = data.get("song").get("chords")
+		    self.chords = []
+		    for i in range(0, len(chord_data)):
+		    	if i < len(chord_data) - 1:
+		    		self.chords.append({"start":chord_data[i]["timestamp"], "end":chord_data[i+1]["timestamp"], "chord":chord_data[i]["chord"]})
+		    	else:
+		    		self.chords.append({"start":chord_data[i]["timestamp"], "end":self.duration, "chord":chord_data[i]["chord"]})
+		    lyric_data = data.get("song").get("lyrics")
+		    self.lyrics = []
+		    for i in range(0, len(lyric_data)):
+		    	if i < len(lyric_data) - 1:
+		    		self.lyrics.append({"start":lyric_data[i]["timestamp"], "end":lyric_data[i+1]["timestamp"], "lyric":lyric_data[i]["lyric"]})
+		    	else:
+		    		self.lyrics.append({"start":lyric_data[i]["timestamp"], "end":self.duration, "lyric":lyric_data[i]["lyric"]})
 
 	def changeCapoTo(self,number):
 		self.capo = number
